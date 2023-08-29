06750dd6808f5a2b5f4e719f0409e36e208e7761
david <cognet.david@hotmail.fr>
2023-08-29 18:29:44 +0200

    add routes&controllers to backend/server.serveur

--- backend/app.js
+++ backend/app.js
@@ -1,23 +1,25 @@
 const express = require("express");
 
 const app = express();
+const userRoutes = require("./routes/user");
+// app.use((req, res, next) => {
+//   console.log("Requête recue");
+//   next();
+// });
 
-app.use((req, res, next) => {
-  console.log("Requête recue");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.status(201);
+//   next();
+// });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+// app.use((req, res, next) => {
+//   res.json({ message: "Votre requête a bien été reçue" });
+// });
 
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue" });
-});
+// app.use((req, res) => {
+//   console.log("Réponse envoyée avec succès !");
+// });
 
-app.use((req, res) => {
-  console.log("Réponse envoyée avec succès !");
-});
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
