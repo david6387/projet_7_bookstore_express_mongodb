a2e2d261f63c26f2b7047ce8a49a1b506b14f6af
david <cognet.david@hotmail.fr>
2023-08-30 19:31:13 +0200

    optimization server.js ports

--- backend/app.js
+++ backend/app.js
@@ -1,25 +1,26 @@
 const express = require("express");
 
 const app = express();
-const userRoutes = require("./routes/user");
-// app.use((req, res, next) => {
-//   console.log("Requête recue");
-//   next();
-// });
+// const userRoutes = require("./routes/user");
+app.use((req, res, next) => {
+  console.log("Requête recue");
+  next();
+});
 
-// app.use((req, res, next) => {
-//   res.status(201);
-//   next();
-// });
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
 
-// app.use((req, res, next) => {
-//   res.json({ message: "Votre requête a bien été reçue" });
-// });
+app.use((req, res, next) => {
+  res.json({ message: "Votre requête a bien été reçue" });
+  next();
+});
 
-// app.use((req, res) => {
-//   console.log("Réponse envoyée avec succès !");
-// });
+app.use((req, res) => {
+  console.log("Réponse envoyée avec succès !!");
+});
 
-app.use("/api/auth", userRoutes);
+// app.use("/api/auth", userRoutes);
 
 module.exports = app;
