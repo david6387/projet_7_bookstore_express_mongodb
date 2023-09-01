fd60163900be36c2af8e0f388ca915d2f56a50cd
david <cognet.david@hotmail.fr>
2023-09-01 19:07:46 +0200

    allow cross-origin requests

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require("express");
 
 const app = express();
+
 // const userRoutes = require("./routes/user");
+
 app.use((req, res, next) => {
-  console.log("Requête recue");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT; DELETE, PATCH, OPTIONS"
+  );
   next();
 });
 
