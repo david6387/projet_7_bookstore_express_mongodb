af5cc0dbbf82292f8b641ce6f6c625f74ccc4bb8
david <cognet.david@hotmail.fr>
2023-09-04 20:52:02 +0200

    mongoose.connect

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,18 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://david:FYZUTb3FYJLGkrFh@clusterbookstore.eqgpj7q.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 // const userRoutes = require("./routes/user");
 
