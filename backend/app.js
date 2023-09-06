007d768dbb3ff0cc552fdbc6febb64d61617b4d8
david <cognet.david@hotmail.fr>
2023-09-06 11:54:02 +0200

    ortho

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,7 @@ app.use((req, res, next) => {
   );
   res.setHeader(
     "Access-Control-Allow-Methods",
-    "GET, POST, PUT; DELETE, PATCH, OPTIONS"
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
   );
   next();
 });
