eb1da5c4e0016a6608949ead8cc9a4b3f4b09c6b
david <cognet.david@hotmail.fr>
2023-09-06 11:45:45 +0200

    routes put&delete(mise à jour&suppression)

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 
 const Book = require("./models/Book");
@@ -29,6 +30,18 @@ app.post("/api/books", (req, res, next) => {
     .catch((error) => res.status(400).json({ error }));
 });
 
+app.put("/api/books/:id", (req, res, next) => {
+  Book.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre modifié" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.delete("/api/books/:id", (req, res, next) => {
+  Book.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Livre supprimé" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
 app.get("/api/books/:id", (req, res, next) => {
   Book.findOne({ _id: req.params.id })
     .then((book) => res.status(200).json(thing))
@@ -53,6 +66,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.status(201);
