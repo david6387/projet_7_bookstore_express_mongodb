bd3c004ac521614cf1da98cc97be088e5e8eb421
david <cognet.david@hotmail.fr>
2023-09-06 11:58:49 +0200

    delete useless test middlewares on app.js

--- backend/app.js
+++ backend/app.js
@@ -69,20 +69,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
-
-app.use((req, res, next) => {
-  res.json({ message: "Votre requête a bien été reçue" });
-  next();
-});
-
-app.use((req, res) => {
-  console.log("Réponse envoyée avec succès !!");
-});
-
 // app.use("/api/auth", userRoutes);
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -37,7 +37,7 @@ const server = http.createServer(app);
 server.on("error", errorHandler);
 server.on("listening", () => {
   const address = server.address();
-  const bind = typeof address === "string" ? "pipe" + address : "port" + port;
+  const bind = typeof address === "string" ? "pipe" + address : "port " + port;
   console.log("Listening on " + bind);
 });
 
