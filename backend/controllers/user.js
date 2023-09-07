e313b9a18f106f605b349fd0255e7189bede7230
david <cognet.david@hotmail.fr>
2023-09-07 16:14:49 +0200

    npm i jsonwebtoken & create auth token

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/User");
 
@@ -38,7 +39,9 @@ exports.login = (req, res, next) => {
             } else {
               res.status(200).json({
                 userId: user._id,
-                token: "TOKEN",
+                token: jwt.sign({ userId: user_id }, "RAMDOM_TOKEN_SECRET", {
+                  expiresIn: "24h",
+                }),
               });
             }
           })
