1f98b5f5004c9f69555ac9b9a7b0b95a776ba40c
david <cognet.david@hotmail.fr>
2023-09-15 13:10:06 +0200

    increase requests in expressRateLimit

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const mongoSanitize = require("express-mongo-sanitize");
 
 const limiter = expressRateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  limit: 200, // Limit each IP to 200 requests per `window` (here, per 15 minutes)
   standardHeaders: "draft-7", // draft-6: `RateLimit-*` headers; draft-7: combined `RateLimit` header
   legacyHeaders: false,
 });
